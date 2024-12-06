da3e794b9c666188c05dae560e85d8a5201dec9c
AbrilSantiso <abrilsantiso@hotmail.com>
2024-12-06 14:08:36 -0300

    fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,21 @@
-import { motion } from 'framer-motion';
-import { styles } from '../styles';
-import { bruno,} from '../assets';
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { bruno } from "../assets";
 
 const Hero = () => {
   return (
     <>
-
-      <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen">
-      </div>
+      <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen"></div>
       <section
         className="relative flex sm:flex-row flex-col w-full h-screen mx-auto 
-        sm:bg-hero bg-hero-mobile overflow-hidden">
+        sm:bg-hero bg-hero-mobile overflow-hidden"
+      >
         <div
           className={`absolute inset-0 sm:top-[250px] top-[150px] 
           lg:top-[150px] xl:top-[250px] ${styles.paddingX} 
           max-w-7xl mx-auto flex flex-row items-start
-          justify-between gap-3`}>
+          justify-between gap-3`}
+        >
           <div className="flex flex-col justify-center items-center mt-5 ml-3">
             <div className="w-5 h-5 rounded-full bg-[#0a0a0a] sm:hidden" />
             <div className="w-1 sm:h-80 h-40 bw-gradient sm:hidden" />
@@ -23,31 +23,35 @@ const Hero = () => {
 
           <div>
             <h1
-              className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}>
-              Bruno{' '}
+              className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}
+            >
+              Bruno{" "}
               <span
-                className="sm:text-battleGray sm:text-[110px] 
-                text-eerieBlack text-[70px] font-mova
-                font-extrabold uppercase">
+                className="text-eerieBlack font-mova font-extrabold uppercase 
+      sm:text-[110px] text-[70px] md:text-[90px] lg:text-[110px]"
+              >
                 Carzedda
               </span>
             </h1>
           </div>
           <div
             className="w-screen flex flex-col items-start 
-            justify-center sm:-ml-[3rem] xxs:mt-4"></div>
+            justify-center sm:-ml-[3rem] xxs:mt-4"
+          ></div>
 
           <div></div>
         </div>
 
         <div
           className="absolute xs:bottom-10 bottom-32 w-full 
-          flex justify-center items-center">
+          flex justify-center items-center"
+        >
           <a href="#about">
             <div
               className="w-[35px] h-[64px] rounded-3xl border-4 
             border-french border-dim flex
-            justify-center items-start p-2">
+            justify-center items-start p-2"
+            >
               <motion.div
                 animate={{
                   y: [0, 24, 0],
@@ -55,7 +59,7 @@ const Hero = () => {
                 transition={{
                   duration: 1.5,
                   repeat: Infinity,
-                  repeatType: 'loop',
+                  repeatType: "loop",
                 }}
                 className="w-3 h-3 rounded-full bg-taupe mb-1"
               />
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -25,30 +25,7 @@ const Reviews = () => {
         -buena- película como decisiones constantes y que nada -por mas que
         parezca- está librado al azar o puesto ahí sin relevancia alguna para
         esta carga simbólica que la sustenta, en este caso, el rol de las
-        ciudades. Viena actúa como nexo entre ellos y la situación que deciden
-        afrontar. En primera instancia, ellos desconocen Viena (Celine admite
-        haber ido de muy chiquita) tal como se desconocen ellos, y en el
-        transcurso de la película iran conociendo la ciudad así como se iran
-        conociendo uno al otro. El terreno es desconocido en todo sentido, hay
-        una decisión en Viena y no una ciudad que ya hayan estado alguno de
-        ellos dos. Uno puede pensar a Viena como una ciudad enigmática y, en
-        cierta parte, inusual para dos personas que no la tenían como primera
-        opción a visitar, este mismo enigma e incertidumbre ante el contexto
-        tambien se ve reflejado en la relación que van formando Jesse y Celine.
-        El no hablar el idioma, el show raro al que le ofrecen asistir unos
-        artistas del lugar aportan a esta enigmática aventura. Me gustó la
-        escena de la vuelta al mundo y, viendo a Viena desde arriba, se dan su
-        primer beso, su relación ya trasciende a la ciudad en donde están. El
-        segundo acto transcurre en Paris, 9 años después, en donde cada uno está
-        en pareja y transitaron su vida y su camino por separado después de esa
-        promesa fallida de reencontrarse 6 meses después en la misma estación de
-        Viena. En esta segunda parte se desarrolla, aunque se siguen conociendo,
-        lo que (no) fue y lo que podría haber sido y que mejor ciudad para
-        representar la nostalgia que Paris, la idealización del pasado es eje
-        central de la película y parte de la esencia parisina (un saludo a
-        Midnight in Paris). Esta idealización del pasado va a estar mas presente
-        en Jesse que se lamenta constantemente por el reencuentro fallido, y no
-        en Celine que tiene una postura un tanto más fría si se quiere.
+        ciudades.
       </motion.p>
       <motion.div variants={fadeIn("", "", 0.1, 1)}>
         <div className="flex justify-center mt-6">
